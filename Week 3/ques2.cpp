3dad0bd2613035c28bb4a79b334b6710e45a2484
ishikabatra <102781891+ishikabatra@users.noreply.github.com>
2022-05-06 10:28:51 +0530

    Update ques2.cpp

--- Week 3/ques2.cpp
+++ Week 3/ques2.cpp
@@ -1,4 +1,8 @@
-#include <stdio.h>
+//Given an unsorted array of integers
+//design an algorithm and implement a program to sort this array using selection sort
+//Your program should also find number of comparisons and number of swaps required.
+    
+ #include <stdio.h>
 
 int main()
 {
